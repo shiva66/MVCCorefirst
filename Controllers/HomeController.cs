33573c5fb5c488514789869fbca5fdcece46c98c
Pontus Håkansson <phhakansson@gmail.com>
2020-11-24 13:53:23 +0100

    'Init'

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCAssinment1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Contact()
+        {
+            
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+        
+    }
+}
